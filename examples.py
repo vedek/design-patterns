010723faea8647616e5b03fca893935aa57c2a0f
vedek <52510362+vedek@users.noreply.github.com>
2026-02-09 10:47:05 +0530

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,7 +1,15 @@
-from perobserver import Sensor, JSONPerobserver
+from perobserver import (
+    Sensor,
+    JSONPerobserver,
+    MemoryPerobserver,
+    SQLitePerobserver
+)
 
 sensor = Sensor("temperature", 25)
+
 sensor.attach(JSONPerobserver("sensor.json"))
+sensor.attach(MemoryPerobserver())
+sensor.attach(SQLitePerobserver())
 
 sensor.value = 30
 sensor.value = 31
